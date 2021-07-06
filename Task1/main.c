314e5b553484a5172c280fd14466210a4cb04728
silvercape <1028280371@qq.com>
2021-07-06 19:09:22 +0800

    2021-7-6

--- Task1/main.c
+++ Task1/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char **argv)
 {
-    if (argc <= 2) {
+    if (argc < 2) {
         printf("Usage: %d parameter is not enough\n", argc);
         exit(1);
     }
 
     FILE *fp = fopen(argv[1], "a+");
-    while (fp) {
-        fread(stdout, sizeof(int), 8, fp);
+    char buf[20];
+    fputs(buf, stdout);
+    size_t num;
+    while (num = fread(buf, sizeof(int), 8, fp)) {
+        printf("%4d\n", num);
+        fputs(buf, stdout);
     }
     
     return 0;
