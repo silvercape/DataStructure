3bd29d1e7cafe6dc8cbed9338a685e8a89e6e85b
silvercape <1028280371@qq.com>
2021-07-18 23:18:35 +0800

    Day04-1

--- TestTask/Task1/Test.java
+++ TestTask/Task1/Test.java
@@ -15,14 +15,22 @@
 *
 ******************************************************************************/
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdIn;
 
 public class Test {
     public static void main(String[] args) {
-        // double x, y;
-        StdOut.println("Please enter two float numbers:");
-        double a[] = {0, 0, 0, 0.25, 0.25, 0.25, 0.25, 0, 0, 0 };
-        int ans = StdRandom.discrete(a);
-        StdOut.println(ans);
+        int[] j = new int[1];
+        for (int i = 0; i < 15; i++) {
+            StdOut.println(ff(j ,i));
+        }
+    }
+    public static int  ff(int[] prev, int n) {
+        if (n == 0) {
+            prev[0] = 1;
+            return 0;
+        }
+        int[] prePrev = new int[1];
+        prev[0] = ff(prePrev, n-1);
+        return prePrev[0] + prev[0];
     }
 }
